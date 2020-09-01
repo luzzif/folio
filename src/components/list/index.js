328d5540f4de779326d5059fdc580d7856b75181
luzzif <fedeluzzi00@gmail.com>
2020-09-02 00:55:48 +0200

    chore: remove unused imports

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, View, Text, FlatList } from "react-native";
 import { Row } from "./row";

66df1dd9c88ff7bddc8a160142d1c94e46ea10fe
f-cazzola <48253630+f-cazzola@users.noreply.github.com>
2020-09-01 14:08:23 +0200

    fix: fix the fix

--- src/actions/portfolio/handlers/eth/index.js
+++ src/actions/portfolio/handlers/eth/index.js
@@ -18,25 +18,25 @@ export const getEthPortfolio = async (address, fiatCurrency, coinGeckoIds) => {
     if (tokens && tokens.length > 0) {
         for (const token of json.tokens) {
             const { symbol, decimals } = token.tokenInfo;
-            if (symbol) {
-                const coinGeckoId = coinGeckoIds[symbol.toLowerCase()];
-                if (!coinGeckoId) {
-                    console.warn(`could not get coingecko id for symbol ${symbol}`);
-                    continue;
-                }
-                const info = await getInfoFromCoinGecko(coinGeckoId, fiatCurrency);
-                if (isCoinDismissedBasedOnInfo(info)) {
-                    continue;
-                }
-                portfolio.push({
-                    symbol,
-                    balance: decimalFromWei(
-                        new Decimal(token.balance),
-                        decimals
-                    ).toFixed(),
-                    info,
-                });
+            if (!symbol) continue;
+            const coinGeckoId = coinGeckoIds[symbol.toLowerCase()];
+            if (!coinGeckoId) {
+                console.warn(`could not get coingecko id for symbol ${symbol}`);
+                continue;
             }
+            const info = await getInfoFromCoinGecko(coinGeckoId, fiatCurrency);
+            if (isCoinDismissedBasedOnInfo(info)) {
+                continue;
+            }
+            portfolio.push({
+                symbol,
+                balance: decimalFromWei(
+                    new Decimal(token.balance),
+                    decimals
+                ).toFixed(),
+                info,
+            });
+
         }
     }
     portfolio.push({
